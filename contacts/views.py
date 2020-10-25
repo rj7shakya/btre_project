6722598bee41967b9de5497e221c8ef9823460ba
rj7shakya <shakyarajad1@gmail.com>
2020-10-25 11:20:09 +0545

    email additions

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -14,10 +15,25 @@ def contact(request):
         realtor_email = request.POST['realtor_email']
 
         # check if user has made inquiry already
-        
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an enquiry')
+                return redirect('/listings/'+listing_id)
+
     
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id, )
         contact.save()
+
+        # send mail
+        # send_mail(
+        #     'Property listing inquiry',
+        #     'there has been an inquiry for '+listing+'sign up to admin panel for more info',
+        #     'rj@gmail.com',
+        #     [realtor_email, 'rj@gmail.com'],
+        #     fail_silently=False
+        # )
 
         messages.success(request, 'Your requesthas been submitted,we will get back to you soon')
         return redirect('/listings/'+listing_id)
